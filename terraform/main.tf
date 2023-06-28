7dffd5df4591a2ca944f0ce57b5efc59d4112186
Iago Ferreira <iago.ferreira@ops.team>
2023-06-27 23:15:30 -0300

    ajust pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,13 +24,12 @@ data "aws_ami" "ami_via_cep" {
 resource "aws_instance" "web" {
   count                       = 2
   ami                         = data.aws_ami.ami_via_cep.id
-  instance_type               = "t3.micro"
+  instance_type               = "t2.micro"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allow_http.id]
   tags = {
     Name = "api_via_cep"
   }
-  key_name             = "curso"
 }
 
 resource "aws_security_group" "allow_http" {
